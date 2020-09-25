4dee55bc117af207604ec97ec35e54b1095269de
Jakub Bogdan <kubab1998@o2.pl>
2020-09-25 13:41:00 +0200

    smsapi client dependency added

---
+++ src/main/java/pl/lodz/p/it/ucisms/services/SmsApiService.java
@@ -0,0 +1,9 @@
+package pl.lodz.p.it.ucisms.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsApiService {
+
+    final static String urlForPlSmsapi = "http://api.smsapi.pl";
+}
